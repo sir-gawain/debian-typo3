cec683ff43a76df36026f1f23a14b6d20e5dbca3
Christian Welzel <gawain@camlann.de>
2012-02-22 23:31:53 +0100

    Imported Upstream version 4.3.0

--- typo3conf/localconf.php
+++ typo3conf/localconf.php
@@ -4,7 +4,7 @@ $TYPO3_CONF_VARS['SYS']['sitename'] = 'New TYPO3 site';
 	// Default password is "joh316" :
 $TYPO3_CONF_VARS['BE']['installToolPassword'] = 'bacb98acf97e0b6112b1d1b650b84971';
 
-$TYPO3_CONF_VARS['EXT']['extList'] = 'tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin';
+$TYPO3_CONF_VARS['EXT']['extList'] = 'version,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin,t3editor,reports';
 
 $typo_db_extTableDef_script = 'extTables.php';
 
